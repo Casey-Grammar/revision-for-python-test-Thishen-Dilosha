c374ab9b1907966f38c8cbf92a26955d358f5929
Thishen-Dilosha <s29621@caseygrammar.vic.edu.au>
2026-03-26 01:07:26 +0000

    Fixed Task3 Test

--- Task3.py
+++ Task3.py
@@ -1,12 +1,6 @@
-# Task 3: Dog Years
-# Assume that a dog year is 7 human years. 
-# Write a program that asks the user
 def main():
-    #Write your code for input here
-    
-
-
-    # End of your code for input here
-
+    dogYears = int(input('Dog years: '))
+    humanage = dogYears * 7
+    print(f"Humans age = {humanage}")
 if __name__ == '__main__':
     main()
