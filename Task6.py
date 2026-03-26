8b3ffaa849f7795c5addc51e9287de038b619681
Thishen-Dilosha <s29621@caseygrammar.vic.edu.au>
2026-03-26 01:21:40 +0000

    Fixed Task6 Test

--- Task6.py
+++ Task6.py
@@ -5,6 +5,14 @@
 
 def main():
     #Write your code here
+    inputMessage = input("Enter line: ")
+    
+    # Check if 'bear' is in the input message
+    if 'bear' in inputMessage:
+        print("There's a bear in there.")
+    else:
+        print("No bears here.")
+
     
         
     
